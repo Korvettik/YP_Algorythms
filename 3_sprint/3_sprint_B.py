12143f3a017dd0181f0732827ecbab1de852056a
Korvettik <korvet1988@yandex.ru>
2025-10-30 17:32:32 +0300

    3B почти

--- 3_sprint/3_sprint_B.py
+++ 3_sprint/3_sprint_B.py
@@ -37,30 +37,56 @@ import random
 # модицифицированная быстрая сортировка
 def partition(array, pivot):
     """логика разделения списка на 2 части"""
+    left = list()  # самые сильные
+    center = list()
+    right = list()  # самые слабые
+
+    #print(f'pivot {pivot}')
+
     # слева будут лучшие, правее худшие
     for user in array:
+        #print(f'user {user}')
         # сравнение по баллам
         if user[1] > pivot[1]:
+            left.append(user)
+        elif user[1] == pivot[1]:
 
-        # сравнение по щтрафам
+            # сравнение по штрафам
+            if user[2] > pivot[2]:
+                left.append(user)
+            elif user[2] == pivot[2]:
 
-        # сравнение по имени
+                # сравнение по имени
+                if user[0] < pivot[0]:
+                    left.append(user)
+                elif user[0] == pivot[0]:
+                    center.append(user)
+
+                elif user[0] > pivot[0]:
+                    right.append(user)
 
 
+            elif user[2] < pivot[2]:
+                right.append(user)
 
+        elif user[1] < pivot[1]:
+            right.append(user)
 
-
+    return left, center, right
 
 
 def quicksort(array):
     """общая логика + слияние"""
+    #print(f'array {array}')
+
+    # массивы с 0 или 1 элементами фактически отсортированы
     if len(array) < 2:
-        return array   # массивы с 0 или 1 элементами фактически отсортированы
+        #print(f'array {array}')
+        return array
     else:
         pivot = random.choice(array)  # берем любого user
         left, center, right = partition(array, pivot)  # запускаем деление на 2 группы слева и справа
         return quicksort(left) + center + quicksort(right)  # обратным ходом склеиваем
-
 
 if __name__ == '__main__':
     n = int(input().strip())  # число участников
@@ -69,4 +95,8 @@ if __name__ == '__main__':
     for user in range(n):
         user_list.append(input().strip().split())  # читаем параметры пользователя
 
-    print(user_list)
+    #print(user_list)
+
+    #print(*quicksort(user_list), sep='\n')
+
+    print(*list(item[0] for item in quicksort(user_list)), sep='\n')
