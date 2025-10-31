8035216717df5532a438ae618f9da51c276a3370
Илья Спирин <korvet1988@yandex.ru>
2025-10-31 14:31:37 +0300

    a

--- 3_sprint/a.py
+++ 3_sprint/a.py
@@ -1,9 +1,8 @@
+# https://contest.yandex.ru/contest/23638/problems/A/#3484683/2020_05_05/cZe9CnUElE
+
 def generate_bracket_sequences(n):
     """
     Генерирует все допустимые скобочные последовательности длины 2*n.
-
-    Args:
-        n: Целое число, половина длины желаемой последовательности.
     """
     result = []
 
@@ -11,24 +10,43 @@ def generate_bracket_sequences(n):
         # Базовый случай: последовательность готова
         if len(curr) == 2 * n:
             result.append(curr)
+            #print(f'result {result}')
             return
 
         # Добавляем открывающую скобку, если есть еще доступные
         if left < n:
-            backtrack(curr + '(', left + 1, right)
+            #print(f'( {left} < {n}')
+            backtrack(curr + '(', left + 1, right)  # тут же редактируем новый список и передаем его дальше рекурсивно,
+            # т.е как-бы копируем и переносим с модификацией состояние из текущей локали в новую рекурсивную
+
+        # После того как мы получили "(())", функция возвращается назад (backtracking) к моменту, где можно было пойти по другому пути (СЮДА)
+        # Возвращаемся к состоянию: curr = "(", left = 1, right = 0
+        # Раньше мы пошли по пути if left < n (добавили вторую '(').
+        # Теперь пробуем другое направление — if right < left:
 
         # Добавляем закрывающую скобку, если она не нарушает правил
         if right < left:
+            #print(f') {right} < {left}')
             backtrack(curr + ')', left, right + 1)
 
+
     backtrack('', 0, 0)
+
     return result
 
-# Пример использования:
-print(generate_bracket_sequences(3))
-# Вывод: ['((()))', '(()())', '(())()', '()(())', '()()()']
+# для n = 2
+
+#         ""
+#        /
+#       "("
+#      /    \
+#   "(("    "()"
+#    /      /
+# "(()"   "()("
+#  /        \
+# "(())"    "()()"
 
 
 
 n = int(input().strip())  # целое число от 0 до 10
-generate_bracket_sequences(n)
+print(*generate_bracket_sequences(n), sep='\n')
