7f7495ae553fa73e5e1379e26f99a80d5f176c89
Korvettik <korvet1988@yandex.ru>
2025-11-04 17:36:19 +0300

    c

--- 3_sprint/c.py
+++ 3_sprint/c.py
@@ -1,11 +1,12 @@
 from collections import deque
 
+
 def common_logic(first_string_s_deque, first_string_s_count, second_string_t_deque):
 
     # базовый случай
     if first_string_s_count == 0:
-        print('True')
-        return  # выход
+        #print('True')
+        return True  # выход
 
     else:
         first_letter = first_string_s_deque.popleft()  # вырезали левый
@@ -13,12 +14,18 @@ def common_logic(first_string_s_deque, first_string_s_count, second_string_t_deq
         # проходимся по всем элементам 2 строки
         while second_string_t_deque:
             second_letter = second_string_t_deque.popleft()
+
             if second_letter == first_letter:
                 first_string_s_count -= 1
-                common_logic(first_string_s_deque, first_string_s_count, second_string_t_deque)
+                result = common_logic(first_string_s_deque, first_string_s_count, second_string_t_deque)
 
+                # если это был последний заход в рекурсию и он был достаточным и успешным
+                # Т.е. True передается обратным ходом по всей цепочке рекурсий
+                if result is True:
+                    return True
 
-
+        # если обратным рекурсивным ходом не получили True и не вышли, то упадем сюда
+        return False
 
 
 
@@ -30,4 +37,7 @@ first_string_s_deque = deque(first_string_s)  # на основе итериру
 second_string_t_deque = deque(second_string_t)  # на основе итерируемого объекта создаем скоростной дек
 first_string_s_count = len(first_string_s)
 
-common_logic(first_string_s_deque, first_string_s_count, second_string_t_deque)
+
+result = common_logic(first_string_s_deque, first_string_s_count, second_string_t_deque)
+print(result)
+
