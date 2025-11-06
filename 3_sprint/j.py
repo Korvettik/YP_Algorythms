6df5f4244e031a2c0a544f28c34223ffe85a68a4
Korvettik <korvet1988@yandex.ru>
2025-11-06 15:52:37 +0300

    j

--- 3_sprint/j.py
+++ 3_sprint/j.py
@@ -1,20 +1,33 @@
 def bubble_sort(n, array):
     #print(n, array)
 
-    status = True
-    while status:
-        before_array = array.copy()
 
+    before_array = array.copy()
+    status = True
+    steps = 0
+
+    while status:
         for i in range(n - 1):
             if array[i] > array[i+1]:
                 second = array[i+1]
                 array[i+1] = array[i]
                 array[i] = second
 
-        print(*array, sep=' ')
+        steps += 1
 
-        if before_array == array:
+        if before_array == array and steps == 1:
+            print(*array, sep=' ')
             status = False
+        elif before_array == array:
+            status = False
+        else:
+            before_array = array.copy()
+            print(*array, sep=' ')
+
+
+
+
+
 
 
 
