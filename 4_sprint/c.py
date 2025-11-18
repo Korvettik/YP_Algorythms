4f9ae7bdd84db2eedfc4cf811156756ef6f691d9
Илья Спирин <korvet1988@yandex.ru>
2025-11-18 14:11:53 +0300

    c

--- 4_sprint/c.py
+++ 4_sprint/c.py
@@ -1,33 +1,40 @@
 from collections import defaultdict
+
+# Для каждой пары символов (s[i], t[i]) проверяем двустороннее соответствие
+# Если символ s уже встречался, он должен соответствовать тому же символу t
+# Если символ t уже встречался, он должен соответствовать тому же символу s
+
+def logic(s, t):
+
+    if len(s) != len(t):
+        return False
+
+    else:
+        s_to_t = dict()
+        t_to_s = dict()
+
+        for char_s, char_t in zip(s, t):
+            # Проверяем соответствие букв s -> t
+            if char_s in s_to_t:
+                if s_to_t[char_s] != char_t:
+                    return False
+            else:
+                s_to_t[char_s] = char_t
+
+            # Проверяем соответствие t -> s
+            if char_t in t_to_s:
+                if t_to_s[char_t] != char_s:
+                    return False
+            else:
+                t_to_s[char_t] = char_s
+
+        return True
+
+
 s = input().strip()  # первая строка
 t = input().strip()  # вторая строка
 
-
-s_len = len(s)
-t_len = len(t)
-
-s_dict = defaultdict(list)
-t_dict = defaultdict(list)
-
-if s_len == t_len:
-    for i in range(s_len):
-        s_dict[s[i]].append(i)
-    #print(s_dict)
-
-    for i in range(t_len):
-        t_dict[t[i]].append(i)
-    #print(t_dict)
-
-    flag = True
-    for indexes in s_dict.values():
-        if indexes not in t_dict.values():
-            print('NO')
-            flag = False
-            break
-    if flag:
-        print('YES')
-
-
-
+if logic(s, t):
+    print('YES')
 else:
     print('NO')
