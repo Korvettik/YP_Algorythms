cb6598e1e78f47e48f6d7b9db96c5e5ae2613d4b
Илья Спирин <korvet1988@yandex.ru>
2025-11-18 17:56:39 +0300

    b в процессе

--- 4_sprint/b.py
+++ 4_sprint/b.py
@@ -1,19 +1,40 @@
-from collections import Counter
+#  раунд отсекается, потому что условие "ничьи" относится не к отдельным играм, а к общему счету очков, набранных в определенном диапазоне раундов.
+
+# 1: 1–0
+# 2: 2–0
+# 3: 2–1
+# 4: 3–1
+# 5: 3–2
+# 6: 3–3 = ничья
+# 7: 3–4
+# 8: 4–4 = ничья
+# 9: 5–4
+# 10: 6–4
+
+from collections import defaultdict, Counter
 
 
 n = int(input().strip())  # количество раундов
 all_rounds_result = input().strip().split(' ')
 # print(all_rounds_result)
 
-left_index = 0
-right_index = len(all_rounds_result)
-while left_index != right_index:
-    together_dict = Counter(all_rounds_result[left_index:right_index])
-    print(f'together_dict {together_dict}')
-    if together_dict['0'] == together_dict['1']:
-        print(len(all_rounds_result[left_index:right_index]))
-        break
-    else:
-        left_index += 1
-        right_index -= 1
+
+# left_index = 0
+# right_index = len(all_rounds_result)
+# while left_index != right_index:
+#     together_dict = Counter(all_rounds_result[left_index:right_index])
+#     print(f'together_dict {together_dict}')
+#     if together_dict['0'] == together_dict['1']:
+#         print(len(all_rounds_result[left_index:right_index]))
+#         break
+#     else:
+#         left_index += 1
+#         right_index -= 1
+
+counter = 0
+round_count_dict = defaultdict(int)
+for item in all_rounds_result:
+    round_count_dict[item] += 1
+
+
 
