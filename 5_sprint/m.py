33cc57abeac3f277a63649e75f833b1073270c80
Илья Спирин <korvet1988@yandex.ru>
2025-12-05 16:25:06 +0300

    5_sprint/m.py

--- 5_sprint/m.py
+++ 5_sprint/m.py
@@ -1,15 +1,13 @@
 def sift_up(heap, idx) -> int:
-    # базовый случай для рекурсии
-    if idx == 1:  #  в куче всего [None, текущий добавленный], т.е. всего 1 элемент. Его не с чем сравнивать и перемещать.
-        return idx
-
-    parent_index = idx // 2  # по формуле находим индекс родительского элемента
-
-    if heap[idx] > heap[parent_index]:
-        heap[idx], heap[parent_index] = heap[parent_index], heap[idx]  # меняем местами, чтобы значение родителя было больше значения ребенка
-        sift_up(heap, parent_index)
-
-
+    while idx > 1:
+        parent = idx // 2
+        if heap[idx] > heap[parent]:
+            # Меняем местами с родителем
+            heap[idx], heap[parent] = heap[parent], heap[idx]
+            idx = parent
+        else:
+            break
+    return idx
 
 
 
