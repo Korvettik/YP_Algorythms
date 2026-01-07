042a907918596c3484289426951a39f23ea8f853
Korvettik <korvet1988@yandex.ru>
2026-01-07 16:34:49 +0300

    e

--- 7_sprint/e.py
+++ 7_sprint/e.py
@@ -2,17 +2,19 @@ x = int(input().strip())  # требуемая сумма
 k = int(input().strip())  # количество номиналов
 nominals = list(set(map(int, input().strip().split())))  # уникальные возможные номиналы
 nominals.sort(reverse=True)
-print(nominals)
 
-counts = 0
-remaining_summ = x
 
-for nominal in nominals:
-    if remaining_summ <= 0:
-        break
+# dp[i] - минимальное количество купюр для набора суммы i
+INF = 10**9
+dp = [INF] * (x + 1)
+dp[0] = 0  # для суммы 0 нужно 0 купюр
 
-    # Сколько можем взять из этой кучи
-    take = min(nominal, remaining_summ)
-    # print(take)
-    total_value += price_per_kg * take
-    remaining_capacity -= take
+# заполняем
+for i in range(1, x + 1):
+    for nominal in nominals:
+        if i - nominal >= 0:
+            dp[i] = min(dp[i], dp[i - nominal] + 1)
+
+# если dp[x] осталось INF, значит сумму набрать невозможно
+result = dp[x] if dp[x] != INF else -1
+print(result)
